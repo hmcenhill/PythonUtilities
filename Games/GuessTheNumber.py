1dd77dd1e974d3476cb619771357f23b410bf922
Hermann McEnhill <hmcenhill@gmail.com>
2022-04-01 11:34:54 -0400

    remove debug noise and replay to guessing game

--- Games/GuessTheNumber.py
+++ Games/GuessTheNumber.py
@@ -23,37 +23,56 @@ def GetNum():
 def GetGuessCount(maxNum):
     guessCount = 1
     square = 2
-    print(str(maxNum), str(square), str(guessCount), sep=' ')
-    print(str(square > maxNum))
 
     while square < maxNum:
-        print(str(maxNum), str(square), str(guessCount), sep=' ')
         square = square * 2
         guessCount = guessCount + 1
     return guessCount
 
+def PlayGame():
+    print("How high would you like the guessing game number to be?")
+    maxNum = GetNum()
+    guessCount = GetGuessCount(maxNum)
+    print("Wow! I'm going to give you " + str(guessCount) + " tries to guess my number. Are you Ready?")
+
+    secretNumber = random.randint(1, maxNum)
+
+    print("Guess a number!")
+
+    for guessNumber in range(1, guessCount + 1):
+        guess = GetNum()
+        if guess == secretNumber:
+            print("You guessed it! You guessed my number in only " + str(guessNumber) + " tries!")
+            break;
+        elif guess < secretNumber:
+            print("Too Low... ", end='')
+        else:
+            print("Too high... ", end='')
+
+        if guessCount - guessNumber > 0:
+            print("Guess again! You have " + str(guessCount - guessNumber) + " guesses left!")
+        else:
+            print("You ran out of guesses! The number I was thinking of was " + str(secretNumber) + "!")
+
+def GetPlayAgainResponse():
+    print("Want to play again? (y/n)")
+    while True:
+        choice = input()
+        if choice == 'y':
+            return True
+        elif choice == 'n':
+            return False
+        else:
+            print("Please enter 'y' or 'n':")
+
+
 print("Hello! What is your name?")
 name = GetName()
-print("Hi "+name+", how high would you like the guessing game number to be?")
-maxNum = GetNum()
-guessCount = GetGuessCount(maxNum)
-print("Wow! I'm going to give you " + str(guessCount) + " tries to guess my number. Are you Ready?")
+print("Hi "+name+".", end=' ')
 
-secretNumber = random.randint(1, maxNum)
+playing = True;
+while playing:
+    PlayGame()
+    playAgain = GetPlayAgainResponse()
+    playing = playAgain
 
-print("Guess a number!")
-
-for guessNumber in range(1, guessCount + 1):
-    guess = GetNum()
-    if guess == secretNumber:
-        print("You guessed it! You guessed my number in only " + str(guessNumber) + " tries!")
-        break;
-    elif guess < secretNumber:
-        print("Too Low... ", end='')
-    else:
-        print("Too high... ", end='')
-
-    if guessCount - guessNumber > 0:
-        print("Guess again! You have " + str(guessCount - guessNumber) + " guesses left!")
-    else:
-        print("You ran out of guesses! The number I was thinking of was " + str(secretNumber) + "!")
